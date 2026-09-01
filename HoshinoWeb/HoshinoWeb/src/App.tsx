c93d9942fd50450773332bc1594c0c334e85c23e
Agustin Casal <agustincasal@isft220.edu.ar>
2026-09-01 09:10:00 -0300

    01-09-26 09:09 Agus
    Paths actualizados.

--- HoshinoWeb/HoshinoWeb/src/App.tsx
+++ HoshinoWeb/HoshinoWeb/src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 // import { AuthProvider } from './context/AuthContext'; // Esto es para la cuenta, lo saque de https://github.com/AgustinCasal3/Fate-Umbrella.ver/blob/main/0%20Fate%20Umbrella.ver%20Web/Fate%20Umbrella.ver/src/context/AuthContext.tsx
 
 import { Home } from './instances/00 home/Home.tsx';  // 01
+import { Login } from './instances/01 login/Login.tsx';  // 02
+import { Register } from './instances/02 register/Register.tsx';  // 03
 
 function Pagina() {
   return (
@@ -33,12 +35,20 @@ const router = createBrowserRouter([
     errorElement: '',
     children: [
       {
-        index: true, // Ruta: / 01
+        index: true, // Ruta: /
         element: <Index />,
       },
       {
         path: 'home', // Ruta: /home
         element: <Home />,
+      },
+      {
+        path: 'login', // Ruta: /login
+        element: <Login />,
+      },
+      {
+        path: 'register', // Ruta: /register
+        element: <Register />,
       },
     ],
   },
