5be5c67473824ae35bec9a4ca1e58df7f1b1305f
Agustin Casal <agustincasal@isft220.edu.ar>
2026-09-01 10:25:10 -0300

    01-09-26 10:24 Agus
    Terminado el header. Con animaciones.

--- HoshinoWeb/HoshinoWeb/src/instances/0 header/Header.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/0 header/Header.tsx
@@ -1,20 +1,24 @@
+import './Header.css';
+
 import GatoHoshino from '../../assets/imgs/0 header/Gato Hoshino.png'
+import GatoHoshinoHover from '../../assets/imgs/0 header/Gato Hoshino Hover.png'
 
 export function Header() {
     return (
         <>
         <header>
             <div className="headerImg">
-                <img src={GatoHoshino} alt="Mascota Hoshino Repair" />
+                <img className='gatoImg' src={GatoHoshino} alt="Mascota Hoshino Repair" />
+                <img className='gatoImgHover' src={GatoHoshinoHover} alt="Mascota Hoshino Repair" />
             </div>
 
-            <div className="headerContenido">
+            <nav className="headerContenido">
                 <p><a href="/home">Inicio</a></p>
                 <p><a href="/tienda">Tienda</a></p>
                 <p><a href="/home">Chatbot</a></p>
                 <p><a href="/reclamos">Reclamos</a></p>
                 <p><a href="/reparaciones">Reparaciones</a></p>
-            </div>
+            </nav>
 
             <div className="headerCuenta">
                 <p><a href="/login">Iniciar Sesion</a></p>
