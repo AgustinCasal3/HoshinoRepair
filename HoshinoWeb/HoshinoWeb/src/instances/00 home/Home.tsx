4580be9218813e6e6c86ce8b0e1f250d1a88a1c0
Agustin Casal <agustincasal@isft220.edu.ar>
2026-09-01 19:38:53 -0300

    01-09-26 19:38 Agus
    Terminado el Home (segun lo prototipado)

--- HoshinoWeb/HoshinoWeb/src/instances/00 home/Home.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/00 home/Home.tsx
@@ -7,6 +7,9 @@ import { useState } from 'react';
 import ImgCarrousel1 from '../../assets/imgs/00 home/ImgCarrousel1.png';
 import ImgCarrousel2 from '../../assets/imgs/00 home/ImgCarrousel2.png';
 
+import ImgProd1 from '../../assets/imgs/tienda/Productos/PC1.png';
+import ImgProd2 from '../../assets/imgs/tienda/Productos/PC2.png';
+
 import { Header } from '../0 header/Header';
 
 export function Home() {
@@ -51,18 +54,50 @@ export function Home() {
                 <div className="homeTituloProductos">
                     <h1>Productos Sugeridos</h1>
                 </div>
-                <div className="homeProductoSugerido">
-                    <div className="homeImgProducto">
-                        <img className="homeImgProducto" src="" alt=""/>
+                <div className="homeContenedorProductos">
+
+                    <div className="homeProductosSugeridos">
+                    
+                        {/* Producto */}
+                        <div className="homeProductoSugerido">
+                            <div className="homeImgProducto">
+                                <img className="homeImgProducto" src={ImgProd1} alt=""/>
+                            </div>
+                            <div className="homeInfoProducto">
+                                <h3 className='homeTituloProducto'>
+                                    PC Gamer Intel i9 9900KS 64GB RAM DDR4 NVIDIA RTX 2080 Ti 2TB M.2 NVME
+                                </h3>
+                                <h2 className="homePrecioProducto">
+                                    $999.999
+                                </h2>
+                            </div>
+                            <div className="homeBotonVerProducto">
+                                <button className="homeBotonVerProducto">Ver</button>
+                            </div>
+                        </div>
+
+                        {/* Producto */}
+                        <div className="homeProductoSugerido">
+                            <div className="homeImgProducto">
+                                <img className="homeImgProducto" src={ImgProd2} alt=""/>
+                            </div>
+                            <div className="homeInfoProducto">
+                                <h3 className='homeTituloProducto'>
+                                    PC Gamer AMD Ryzen 5 7600 16GB RAM DDR5 NVIDIA RTX 3060 Ti 1TB M.2 NVME
+                                </h3>
+                                <h2 className="homePrecioProducto">
+                                    $799.999
+                                </h2>
+                            </div>
+                            <div className="homeBotonVerProducto">
+                                <button className="homeBotonVerProducto">Ver</button>
+                            </div>
+                        </div>
+                        
                     </div>
-                    <div className="homeInfoProducto">
-                        <h3 className='homeTituloProducto'></h3>
-                        <h2 className="homePrecioProducto"></h2>
-                    </div>
-                    <div className="homeBotonVerProducto">
-                        <button className="homeBotonVerProducto">Ver</button>
-                    </div>
+
                 </div>
+
             </section>
         </>
     )
