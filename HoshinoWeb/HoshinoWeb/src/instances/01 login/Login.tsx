611e795b5020230429bd505bc74c9148aca3339d
Tobias Noba <tobiass.nobafioriti@gmail.com>
2026-09-01 16:45:16 -0300

    1-09-26 tobias
    Termine la primera versión de la pagina de registro

--- HoshinoWeb/HoshinoWeb/src/instances/01 login/Login.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/01 login/Login.tsx
@@ -1,4 +1,4 @@
-import './Login.css'
+import './Login.css';
 
 export function Login() {
     return (
@@ -8,33 +8,34 @@ export function Login() {
         <div className='loginContenedor'>
 
             {/* Form del login (el cuadrado gris para adentro)*/}
-                <form className='loginForm'>
+            <form className='loginForm'>
 
-                    {/* Div con todo el contenido del login */}
-                    <div className='loginDiv'>
+                {/* Div con todo el contenido del login */}
+                <div className='loginDiv'>
 
-                        {/* Titulo del formulario de login */}
-                        <h1 className='loginTitulo'>inicio de sesión</h1>
+                    {/* Titulo del formulario de login */}
+                    <h1 className='loginTitulo'>inicio de sesión</h1>
 
-                        {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
-                        <div className='loginCampos'>
-                            <label className='loginLabel' htmlFor="email">Email:</label>
-                            <input className='loginInput' type="email" required minLength={5} id='email' name='email' />
-                        </div>
-
-                        {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
-                        <div className='loginCampos'>
-                            <label className='loginLabel' htmlFor="password">Contraseña:</label>
-                            <input className='loginInput' type="password" required minLength={8} id='password' name='password'/>
-                            <a className='loginLink' href="">¿Olvidaste tu contraseña?</a>
-                        </div>
-
-                        {/* Boton que envia el formulario de inicio de sesión */}
-                        <button className='loginButton' type='submit'>Iniciar sesión</button>
-
-                        <a className='loginLink' href="">¿aun no tenes una cuenta?</a>
+                    {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
+                    <div className='loginCampos'>
+                        <label className='loginLabel' htmlFor="email">Email:</label>
+                        <input className='loginInput' type="email" required minLength={5} id='email' name='email' />
                     </div>
-                </form>
+
+                    {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
+                    <div className='loginCampos'>
+                        <label className='loginLabel' htmlFor="password">Contraseña:</label>
+                        <input className='loginInput' type="password" required minLength={8} id='password' name='password'/>
+                        <a className='loginLink' href="">¿Olvidaste tu contraseña?</a>
+                    </div>
+
+                    {/* Boton que envia el formulario de inicio de sesión */}
+                    <button className='loginButton' type='submit'>Iniciar sesión</button>
+
+                    {/* Anchor para llevar a la pagina de registro /register */}
+                    <a className='loginLink' href="/register">¿aun no tenes una cuenta?</a>
+                </div>
+            </form>
         </div>
         </>
     )
--- HoshinoWeb/HoshinoWeb/src/instances/02 register/Register.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/02 register/Register.tsx
@@ -1,6 +1,47 @@
+import './Register.css';
+
 export function Register() {
     return (
         <>
+
+        {/*Div contenedor del registro, el que lo acomoda en el medio*/}
+        <div className='registerContenedor'>
+
+            {/* Form del registro (el cuadrado gris para adentro)*/}
+            <form className='registerForm'>
+
+                {/* Div con todo el contenido del registro */}
+                <div className='registerDiv'>
+
+                    {/* Titulo del formulario de registro */}
+                    <h1 className='registerTitulo'>Registrarse</h1>
+
+                    {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
+                    <div className='registerCampos'>
+                        <label className='registerLabel' htmlFor="email">Email:</label>
+                        <input className='registerInput' type="email" required minLength={5} id='email' name='email'/>
+                    </div>
+
+                    {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
+                    <div className='registerCampos'>
+                        <label className='registerLabel' htmlFor="password">Contraseña:</label>
+                        <input className='registerInput' type="password" required minLength={8} id='password' name='password'/>
+                    </div>
+
+                    {/* Div para que juntar las partes de los campos y que tengan el mismo margin */}
+                    <div className='registerCampos'>
+                        <label className='registerLabel' htmlFor="repeatpassword">Repetir contraseña:</label>
+                        <input className='registerInput' type="password" required minLength={8} id='repeatpassword' name='repeatpassword'/>
+                    </div>
+
+                    {/* Boton que envia el formulario de registro (Crear cuenta) */}
+                    <button className='registerButton'>Crear cuenta</button>
+
+                    {/* Anchor para llevar a la pagina de login /login */}
+                    <a className='registerLink' href="/login">¿Ya tenes una cuenta? iniciar sesión</a>
+                </div>
+            </form>
+        </div>
         </>
     )
 }
