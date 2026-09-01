2d67e1df8e442b935ae915e7bf02d81563307582
Tobias Noba <tobiass.nobafioriti@gmail.com>
2026-09-01 11:24:51 -0300

    1-09-26 tobias
    Primera version de la pantalla del login

--- HoshinoWeb/HoshinoWeb/src/instances/01 login/Login.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/01 login/Login.tsx
@@ -3,6 +3,25 @@ import './Login.css'
 export function Login() {
     return (
         <>
+        {/*Div contenedor de el login*/}
+        <div className='loginContenedor'>
+
+            {/* Div del login */}
+            <div>
+
+                <form className='login' action="">
+                    <div className="loginTitulo">
+                        <h1>inicio de sesión</h1>
+                    </div>
+                    <label htmlFor="">Email:</label>
+                    <input className='loginInput' type="text" />
+                    <label htmlFor="">Contraseña:</label>
+                    <input className='loginInput' type="text" />
+                    <a href="">¿Olvidaste tu contraseña?</a>
+                    <button className='loginButton'>Inciar sesión</button>
+                </form>
+            </div>
+        </div>
         </>
     )
 }
