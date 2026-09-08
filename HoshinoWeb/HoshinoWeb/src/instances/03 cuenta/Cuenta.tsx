4d5ddd6f2d4bd978390465beb1c582025d68ea36
AgustinCasal3 <agustincasal@isft220.edu.ar>
2026-09-08 11:56:09 -0300

    08-09-26 11:55 Agus
    Terminado el boceto de Cuenta.

--- HoshinoWeb/HoshinoWeb/src/instances/03 cuenta/Cuenta.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/03 cuenta/Cuenta.tsx
@@ -2,6 +2,9 @@ import './Cuenta.css';
 
 import UserPfp from '../../assets/imgs/04 cuenta/UserPfp1.png';
 
+import ImgDispositivo1 from '../../assets/imgs/05 dispositivo/productos/Producto1.png'
+import ImgDispositivo2 from '../../assets/imgs/05 dispositivo/productos/Producto2.png'
+
 import { Header } from '../0 header/Header';
 
 export function Cuenta() {
@@ -10,6 +13,8 @@ export function Cuenta() {
     let FechaIngreso = '12/03/2025';
     let DispositivosReparacion = 2;
     let ReservasActivas = 1;
+
+    let numDispositivo = 1;
     
     return (
         <>
@@ -27,6 +32,52 @@ export function Cuenta() {
                         <h3>Reservas Activas: {ReservasActivas}</h3>
 
                         <a href="/reservas">Reservas</a>
+                    </div>
+                </div>
+            </section>
+
+            <section className="cuentaDispositivos">
+                <div className="cuentaTituloDispositivos">
+                    <h1>Tus Dispositivos</h1>
+                </div>
+                <div className="cuentaListaDispositivos">
+                    <div className="cuentaArticuloDispositivo">
+                        <div className="cuentaInfoDispositivo">
+                            <a href={`dispositivo/${numDispositivo}`}>
+                                <img src={ImgDispositivo1} alt={`Foto del dispositivo ${numDispositivo}`} />
+                            </a>
+                            <a href={`dispositivo/${numDispositivo}`}>
+                                <div className="cuentaDatosDispositivo">
+                                    <h3>Notebook Gamer Lenovo</h3>
+                                    <h2>#000001</h2>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="cuentaVerDispositivo">
+                            <a href={`dispositivo/${numDispositivo}`}>
+                                <p>Ver</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="cuentaArticuloDispositivo">
+                        <div className="cuentaInfoDispositivo">
+                            
+                            <a href={`dispositivo/${numDispositivo}`}>
+                                <img src={ImgDispositivo2} alt={`Foto del dispositivo ${numDispositivo}`} />
+                            </a>
+                            <a href={`dispositivo/${numDispositivo}`}>
+                                <div className="cuentaDatosDispositivo">
+                                    <h3>Notebook Gamer Lenovo</h3>
+                                    <h2>#000001</h2>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="cuentaVerDispositivo">
+                            <a href={`dispositivo/${numDispositivo}`}>
+                                <p>Ver</p>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
