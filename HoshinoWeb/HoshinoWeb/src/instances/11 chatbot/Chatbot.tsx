b4aa26cf318a3af4e05c5e0684c5cdbcc3c39d2d
tobias noba <tobiass.nobafioriti@gmail.com>
2026-09-22 10:11:45 -0300

    22/09/26 10:11 tobias
    termine la primera version del chatbot (faltan comentarios)

--- HoshinoWeb/HoshinoWeb/src/instances/11 chatbot/Chatbot.tsx
+++ HoshinoWeb/HoshinoWeb/src/instances/11 chatbot/Chatbot.tsx
@@ -1,6 +1,10 @@
 //Importaciones del archivo Chatbot.tsx (del login)
 import './Chatbot.css';
 
+import flechaEnviar from '../../assets/imgs/11 chatbot/ArrowRight.svg';
+import botonCerrar from '../../assets/imgs/11 chatbot/Close.svg';
+
+
 import {Home} from '../00 home/Home';
 
 //Funcion Chatbot
@@ -16,14 +20,34 @@ export function Chatbot() {
 
                 {/* div con todo el contenido del chatbot */}
                 <div className='chatbotContenido'>
-                    <div className='chatbotDivTitulo'>
-                        <h2 className='chatbotTitulo'>Chatbot: Hoshi :3</h2>
-                        <button className='chatbotButtonCerrar' >X</button>
-                    </div>
 
-                    <div className='chatbotInputButton'>
-                        <input className='chatbotInput' type="text" placeholder="Escribe un mensaje..." />
-                        <button className='chatbotButtonEnviar'>&gt;</button>
+
+                    <div className='chatbotDivTitulo'>
+
+
+                        <h2 className='chatbotTitulo'>Chatbot: Hoshi :3</h2>
+
+
+                        <a href='/home' className='chatbotButtonCerrar' >
+                            <img src={botonCerrar} alt="X" />
+                        </a>
+                    </div>
+                    
+                    <div className='chatbotChat'>
+
+                        <div className='chatbotDivMensajes'>
+                        </div>
+                        
+
+                        <div className='chatbotDivEnviar'>
+
+                            <input className='chatbotInput' type="text" placeholder="Escribe un mensaje..." />
+                            
+                            <a className='chatbotButtonEnviar'>
+                                <img src={flechaEnviar} alt=">" />
+                            </a>
+                        </div>
+
                     </div>
                     
                 </div>
